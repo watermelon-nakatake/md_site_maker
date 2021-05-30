3a8f65749b9e22065390fb75789aaf5d66410669
watermelon-nakatake <kirishima3260@yahoo.co.jp>
2021-05-30 15:33:24 +0900

    after make rewrite app

--- str_counter_md.py
+++ str_counter_md.py
@@ -93,7 +93,7 @@ def read_this_title_log(md_path, pd):
 
 
 if __name__ == '__main__':
-    target_md = 'reibun/md_files/pc/majime/m0_4_hm.md'
+    target_md = 'reibun/md_files/pc/majime/m0_dt_hm.md'
     # query_check_and_make_html.check_single_page_seo(28, target_md, False)
     # read_this_title_log(target_md, reibun.main_info.info_dict)
     key_list = ['長文', 'ハッピーメール']
