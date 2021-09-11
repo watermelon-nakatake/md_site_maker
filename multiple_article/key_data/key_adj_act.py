26a29e6b9a142e68cfa15b0c187f348bbee09019
watermelon-nakatake <kirishima3260@yahoo.co.jp>
2021-09-11 09:02:21 +0900

    before make add act

---
+++ multiple_article/key_data/key_adj_act.py
@@ -0,0 +1,8 @@
+keyword_dict = {
+    0: {'act': '不倫する', 'act_noun': '不倫', 'act_noun_flag': False, '2act_w': 'セックスしたい', '2act_noun': 'セックス',
+        'act_connection': ['不倫', '不倫関係', '婚外恋愛'], }
+}
+
+a_adj_dict = {
+    0: {'a_adj': 'SNSで'}
+}
